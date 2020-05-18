91bb59bb5545082b9eff5c5bff8ef2892da3a8e4
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2020-05-18 22:34:07 +0530

    fix: dev: master: subcommand on structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "nvm", about = "nvim")]
+struct Opt {
+  #[structopt(subcommand)]
+  commands: Option<Command>
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "command", about = "command")]
+enum Command {
+  #[structopt(name = "local")]
+  Local(LocalOpts)
+}
+
+#[derive(Debug, StructOpt)]
+struct LocalOpts {
+  version: u8
+}
+
 fn main() {
-    println!("Hello, world!");
+  let opt = Opt::from_args();
+  println!("{:?}", opt);
+  handle_subcommand(opt);
+}
+
+fn handle_subcommand(opt: Opt) {
+  if let Some(subcommand) = opt.commands{
+    match subcommand {
+      Command::Local(cfg) => {
+        println!("handle Local: {:?}", cfg);
+      }
+    }
+  }
 }
