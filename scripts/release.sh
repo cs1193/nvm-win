1838dd59036835f2f9e25234f17e68e80af99b87
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2020-05-21 21:51:37 +0530

    fix: dev: master: bumped  version

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,18 @@
 
 version = $1
 
-cargo bump $1 --git-tag
-git add .
-git commit -m "fix: dev: master: bumped version"
-git push -u origin master --tags
+if [ -z "$version" ]
+then
+    echo "Bumping to $version"
+    cargo bump $version --git-tag
+
+    echo "Adding files to git"
+    git add .
+    git commit -m "fix: dev: master: bumped $version version"
+    git push -u origin master --tags
+
+    echo "Publishing the package"
+    cargo publish
+else 
+    echo "Please provide version <major|minor|patch>"
+fi
